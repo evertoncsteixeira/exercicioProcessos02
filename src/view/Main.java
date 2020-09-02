6e4b2bf760f8c88aa92d591a209f9dc8a1ee267f
Everton <Everton@EVERTON>
2020-09-02 10:57:02 -0300

    ExercicioProcessos02

---
+++ src/view/Main.java
@@ -0,0 +1,13 @@
+package view;
+import controller.ProcessosOs;
+import javax.swing.JOptionPane;
+public class Main {
+
+	public static void main(String[] args) {
+		ProcessosOs Processos = new ProcessosOs();	
+		Processos.lerProcessos(Processos.retornaOs());
+		Processos.finalProcessoNome(JOptionPane.showInputDialog("Digite o nome do processo para ser excluído"), Processos.retornaOs());
+		Processos.finalProcessoPID(JOptionPane.showInputDialog("Digite o PID do processo para ser excluído"), Processos.retornaOs());
+	}
+
+}
